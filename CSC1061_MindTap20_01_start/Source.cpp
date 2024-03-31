1abbb9af4c70abb4d966d86e139a0b6f556c5f53
kmgei <kmgei@DORI>
2024-03-31 13:08:05 -0600

    Add project files.

---
+++ CSC1061_MindTap20_01_start/Source.cpp
@@ -0,0 +1,17 @@
+// CSC1061_MindTap20_01
+// Depth First Traversal
+#include <iostream>
+#include <fstream>
+#include "graphType.h" 
+
+using namespace std;
+
+int main()
+{
+	// Instantiate one graphType object
+	// Create an array of graphType objects
+	// Complete a depth first traversal
+	// Complete a breadth first traversal
+
+	return 0;
+}
